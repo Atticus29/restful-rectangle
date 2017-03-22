0c4065c10fe233281389822d887c068ce2a0aed9
Chris Wainner and Mark Fisher <chris@>
2017-03-22 08:28:20 -0700

    add objects, their methods, and their accompanying tests

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -2,5 +2,16 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class RectangleTest {
+  @Test
+  public void all_returnsAllInstancesOfRectangle_true(){
+    Rectangle firstRectangle = new Rectangle(10, 20);
+    Rectangle secondRectangle = new Rectangle(5, 5);
+    assertTrue(Rectangle.all().contains(firstRectangle));
+    assertTrue(Rectangle.all().contains(secondRectangle));
+  }
 
+  @Test public void area_returnsTheAreaOfTheRectangle_450(){
+    Rectangle testRectangle = new Rectangle(15, 30);
+    assertEquals(450, testRectangle.area());
+  }
 }
